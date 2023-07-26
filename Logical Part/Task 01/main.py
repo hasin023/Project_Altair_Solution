4581d34cde75a390a389510bc9d9b2dda2b60efe
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-26 19:48:13 +0600

    Added Logical part task 01 code

---
+++ Logical Part/Task 01/main.py
@@ -0,0 +1,43 @@
+# The vertices that have been visited are true, and the one's not visited are false
+# 0 is initially true as it is the starting point,
+# so it has already been visited at least once at the beginning
+vertices = {
+    0: True,
+    1: False,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+    6: False
+}
+
+# edges = [[0, 1], [1, 2], [2, 0]]
+# edges = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]]
+edges = [[0, 1], [1, 2], [2, 0], [3, 4], [4, 5], [5, 6], [6, 3]]
+
+
+for edge in edges:
+    # if both 0 and 1 exist in the same array inside the edges list,
+    # then change the value of 1 to true, since 0 is true
+    # because they being in the same array/group means they have an edge in between them
+    # thus, they can both be visited
+    if edge[0] in vertices and edge[1] in vertices:
+
+        if vertices[edge[0]] is True:
+            vertices[edge[1]] = True
+
+# showing the individual visited status of the vertices
+print(vertices)
+
+# Check if all values are True
+all_values_true = all(vertices.values())
+
+# Printing the final result
+if all_values_true is True:
+    print("true")
+else:
+    print("false")
+
+
+
+
