798a9f6d1458b5d9a1aee37e5b7f0949d403fabe
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-27 15:58:43 +0600

    Updated python file

--- Logical Part/Task 01/main.py
+++ Logical Part/Task 01/main.py
@@ -1,6 +1,26 @@
-# The vertices that have been visited are true, and the one's not visited are false
-# 0 is initially true as it is the starting point,
-# so it has already been visited at least once at the beginning
+""" The vertices that have been visited are true, and the one's not visited are false
+    0 is initially true as it is the starting point,
+    so it has already been visited at least once at the beginning """
+
+""" For example 1
+    vertices = {
+    0: True,
+    1: False,
+    2: False
+}"""
+
+
+""" For example 2
+    vertices = {
+    0: True,
+    1: False,
+    2: False,
+    3: False,
+    4: False,
+    5: False
+}"""
+
+
 vertices = {
     0: True,
     1: False,
@@ -11,16 +31,19 @@ vertices = {
     6: False
 }
 
+# For example 1
 # edges = [[0, 1], [1, 2], [2, 0]]
+# For example 2
 # edges = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]]
+
 edges = [[0, 1], [1, 2], [2, 0], [3, 4], [4, 5], [5, 6], [6, 3]]
 
 
 for edge in edges:
-    # if both 0 and 1 exist in the same array inside the edges list,
-    # then change the value of 1 to true, since 0 is true
-    # because they being in the same array/group means they have an edge in between them
-    # thus, they can both be visited
+    """ Only if both 0 and 1 exist in the same array inside the edges list,
+    then change the value of 1 to true, since 0 is true
+    because they being in the same array/group means they have an edge in between them
+    thus, they can both be visited """
     if edge[0] in vertices and edge[1] in vertices:
 
         if vertices[edge[0]] is True:
@@ -37,7 +60,3 @@ if all_values_true is True:
     print("true")
 else:
     print("false")
-
-
-
-
